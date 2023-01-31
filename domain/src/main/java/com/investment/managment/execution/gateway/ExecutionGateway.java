a10dfb3136729f2cb7b8efeadf37e4fd5d457b85
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-30 22:45:52 -0300

    feat: :sparkles: create contract of findById

--- domain/src/main/java/com/investment/managment/execution/gateway/ExecutionGateway.java
+++ domain/src/main/java/com/investment/managment/execution/gateway/ExecutionGateway.java
@@ -3,6 +3,10 @@ package com.investment.managment.execution.gateway;
 import com.investment.managment.execution.Execution;
 import com.investment.managment.execution.ExecutionID;
 
+import java.util.Optional;
+
 public interface ExecutionGateway {
     Execution create(Execution anExecution);
+
+    Optional<Execution> findById(ExecutionID anId);
 }
