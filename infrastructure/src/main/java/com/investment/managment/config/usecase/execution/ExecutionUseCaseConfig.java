255e20d9fc6d9a995a322b14fd02d99687835ba8
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-30 23:10:20 -0300

    feat: :sparkles: create Beans about updateExecutionUseCase and deleteExecutionByIdUseCase

--- infrastructure/src/main/java/com/investment/managment/config/usecase/execution/ExecutionUseCaseConfig.java
+++ infrastructure/src/main/java/com/investment/managment/config/usecase/execution/ExecutionUseCaseConfig.java
@@ -1,7 +1,9 @@
 package com.investment.managment.config.usecase.execution;
 
 import com.investment.managment.execution.create.CreateExecutionUseCase;
+import com.investment.managment.execution.deleteById.DeleteExecutionByIdUseCase;
 import com.investment.managment.execution.gateway.ExecutionGateway;
+import com.investment.managment.execution.update.UpdateExecutionUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,16 @@ public class ExecutionUseCaseConfig {
     @Bean
     public CreateExecutionUseCase createExecutionUseCase(){
         return new CreateExecutionUseCase(this.executionGateway);
+    }
+
+    @Bean
+    public UpdateExecutionUseCase updateExecutionUseCase(){
+        return new UpdateExecutionUseCase(this.executionGateway);
+    }
+
+    @Bean
+    public DeleteExecutionByIdUseCase deleteExecutionByIdUseCase(){
+        return new DeleteExecutionByIdUseCase(this.executionGateway);
     }
 
 }
