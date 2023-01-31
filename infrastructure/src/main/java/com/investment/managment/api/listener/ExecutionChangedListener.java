e96cc957944ef416a23c0ae23117225e9762c2cb
joseMarciano <marcianojosepaulo@gmail.com>
2023-01-30 23:11:03 -0300

    feat: :sparkles: put delete and update execution in listener

--- infrastructure/src/main/java/com/investment/managment/api/listener/ExecutionChangedListener.java
+++ infrastructure/src/main/java/com/investment/managment/api/listener/ExecutionChangedListener.java
@@ -3,20 +3,33 @@ package com.investment.managment.api.listener;
 import com.investment.managment.execution.ExecutionChangeReason;
 import com.investment.managment.execution.create.CreateExecutionCommandInput;
 import com.investment.managment.execution.create.CreateExecutionUseCase;
+import com.investment.managment.execution.deleteById.DeleteExecutionByIdCommandInput;
+import com.investment.managment.execution.deleteById.DeleteExecutionByIdUseCase;
 import com.investment.managment.execution.models.ExecutionChangedRequest;
+import com.investment.managment.execution.update.UpdateExecutionCommandInput;
+import com.investment.managment.execution.update.UpdateExecutionUseCase;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+import static com.investment.managment.execution.models.ExecutionChangedRequest.ExecutionDTO;
 import static java.util.Optional.ofNullable;
 
 @Component
 public class ExecutionChangedListener {
 
     private final CreateExecutionUseCase createExecutionUseCase;
+    private final UpdateExecutionUseCase updateExecutionUseCase;
+    private final DeleteExecutionByIdUseCase deleteExecutionByIdUseCase;
 
-    public ExecutionChangedListener(final CreateExecutionUseCase createExecutionUseCase) {
+    public ExecutionChangedListener(final CreateExecutionUseCase createExecutionUseCase,
+                                    final UpdateExecutionUseCase updateExecutionUseCase,
+                                    final DeleteExecutionByIdUseCase deleteExecutionByIdUseCase) {
         this.createExecutionUseCase = createExecutionUseCase;
+        this.updateExecutionUseCase = updateExecutionUseCase;
+        this.deleteExecutionByIdUseCase = deleteExecutionByIdUseCase;
     }
 
     @SqsListener(value = "${aws.sqs.execution-event-changed-queue}")
@@ -39,7 +52,7 @@ public class ExecutionChangedListener {
     }
 
     private void resolveExecutionCreated(final ExecutionChangedRequest request) {
-        ofNullable(request.execution())
+        getExecution(request)
                 .ifPresent(execution -> {
                     createExecutionUseCase.execute(CreateExecutionCommandInput.with(
                             execution.id(),
@@ -55,12 +68,34 @@ public class ExecutionChangedListener {
                 });
     }
 
+
     private void resolveExecutionUpdated(final ExecutionChangedRequest request) {
-        //todo: need to be implemented
+        getExecution(request)
+                .ifPresent(execution -> {
+                    updateExecutionUseCase.execute(UpdateExecutionCommandInput.with(
+                            execution.id(),
+                            execution.origin(),
+                            execution.stockId(),
+                            execution.walletId(),
+                            execution.profitPercentage(),
+                            execution.executedQuantity(),
+                            execution.executedPrice(),
+                            execution.executedVolume(),
+                            execution.status()
+                    ));
+                });
     }
 
     private void resolveExecutionDeleted(final ExecutionChangedRequest request) {
-        //todo: need to be implemented
+        getExecution(request)
+                .ifPresent(execution -> {
+                    this.deleteExecutionByIdUseCase.execute(DeleteExecutionByIdCommandInput.with(execution.id()));
+                });
+    }
+
+
+    private static Optional<ExecutionDTO> getExecution(final ExecutionChangedRequest request) {
+        return ofNullable(request.execution());
     }
 
 }
