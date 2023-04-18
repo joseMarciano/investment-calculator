513cb130d9db572d9b41f52d09a6e0f3cbb965f6
joseMarciano <marcianojosepaulo@gmail.com>
2023-04-17 22:02:14 -0300

    chore: :wrench: put cron schedule

--- infrastructure/src/main/java/com/investment/managment/api/stock/scheduler/StockAPIScheduleTasksImpl.java
+++ infrastructure/src/main/java/com/investment/managment/api/stock/scheduler/StockAPIScheduleTasksImpl.java
@@ -126,7 +126,6 @@ public class StockAPIScheduleTasksImpl implements StockAPIScheduleTasks {
      * Each 25 minutes 9am until 6pm on weekdays
      */
     @Override
-//    @Scheduled(cron = "0 */25 9-18 * * MON-FRI", zone = "America/Sao_Paulo")
     @Scheduled(cron = "0 */1 9-23 * * MON-FRI", zone = "America/Sao_Paulo")
     public void updateLastTradePrice() {
         final var usedStocks = new ArrayList<>(this.stockGateway.findUsedStocks());
